2bbb3e18f125d9a855ab7e5b18340572b08067ee
bangHasan <banghasan@gmail.com>
2026-02-03 12:18:39 +0700

    chore: add warning log when tokenizer not found for model in count_tokens function

--- pageindex/utils.py
+++ pageindex/utils.py
@@ -30,6 +30,10 @@ def count_tokens(text, model=None):
     try:
         enc = tiktoken.encoding_for_model(model)
     except KeyError:
+        logging.warning(
+            "Tokenizer tidak ditemukan untuk model '%s'. Menggunakan fallback cl100k_base.",
+            model,
+        )
         enc = tiktoken.get_encoding("cl100k_base")
     tokens = enc.encode(text)
     return len(tokens)
